8d5afb6c48368c9fad129bf9800636d2130675d5
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-24 13:29:09 +0100

    Update Fish.cs

--- Fish.cs
+++ Fish.cs
@@ -8,35 +8,28 @@ namespace Prvi_dan_mono
 {
     internal class Fish : Animals, IAnimalActions
     {
-       
-       
-      
-    
         public void Sound()
         {
             System.Console.WriteLine("Blup");
-
         }
+        
         public void Eat(int amount) {
             weight += amount / 2;
         }
+        
         public Fish(string inName, int inWeight, double inHeight)
         {
             weight = inWeight;
             height = inHeight;
             name = inName;
-
         }
+        
         public unsafe void swim() {
             int meters=10;
             int* pointer = &meters;
 
             System.Console.WriteLine(*pointer);
-        
-        }
-
-
-
         }
     }
+}
 
