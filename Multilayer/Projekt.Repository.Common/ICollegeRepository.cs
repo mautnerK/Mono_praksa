13d0755addb2bbd7cbc764ed6b6226346dd1b4c7
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-27 14:56:34 +0100

    Add files via upload

---
+++ Multilayer/Projekt.Repository.Common/ICollegeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Model;
+namespace Project.Repository.Common
+{
+    public interface ICollegeRepository
+    {
+        List<College> GetAllColleges();
+        College GetCollegeById(int id);
+
+        void SaveNewValue(College college);
+
+        void UpdateCollegeAddress(int id, string address);
+
+        void RemoveCollegeById(int id);
+    }
+}
---
+++ Multilayer/Projekt.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repository.Common
+{
+    public interface IStudentRepository
+    {
+       List<Student> GetAllStudents();
+
+       Student GetAStudent(int id);
+
+       void SaveNewValue(Student student);
+
+       void UpdateStudentAddress(int id, string address);
+
+       void RemoveStudentById(int id);
+    }
+}
