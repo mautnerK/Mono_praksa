aa4f4e33dbbe9626199c11e4a6fb223fd943aafa
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-27 15:17:48 +0100

    Add files via upload

---
+++ Multilayer/Projekt.Model/Student.cs
@@ -0,0 +1,22 @@
+﻿using Project.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model
+{
+    public class Student : IStudent
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Surname { get; set; }
+
+        public string Address { get; set; }
+
+        public int CollegeId { get; set; }
+    }
+}
