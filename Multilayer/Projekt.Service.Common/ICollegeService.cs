559befd995563b82c2408558b99515026eedaba0
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-27 15:16:17 +0100

    Add files via upload

---
+++ Multilayer/Projekt.Service.Common/ICollegeService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Model;
+namespace Project.Service.Common
+{
+    public interface ICollegeService
+    {
+        List<College> GetAllColleges();
+        College GetCollegeById(int id);
+
+        void SaveNewValue(College college);
+
+        void UpdateCollegeAddress(int id, string address);
+
+        void RemoveCollegeById(int id);
+    }
+}
