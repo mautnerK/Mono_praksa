b6284ee7ad418c982e30a5b5eaf8c1f89e0d832d
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-27 14:58:38 +0100

    Add files via upload

---
+++ Multilayer/webapi_projekt/Controllers/CollegeController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using Project.Service.Common;
+using Project.Service;
+using Project.Model;
+namespace web_api_project.Controllers
+{
+   
+
+    public class CollegeController : ApiController
+    {
+        public CollegeController() { }
+        protected ICollegeService service = new CollegeService();
+
+        [HttpGet]
+        public List<College> GetAllColleges()
+        {
+            return service.GetAllColleges();
+        }
+        [HttpGet]
+        public College GetCollegeById(int id)
+        {
+            return service.GetCollegeById(id);
+        }
+
+        [HttpPost]
+        public void SaveNewValue([FromBody] College college)
+        {
+            service.SaveNewValue(college);
+        }
+
+        [HttpPut]
+        public void UpdateCollegeAddress(int id, string address)
+        {
+            service.UpdateCollegeAddress(id, address);  
+        }
+
+        [HttpDelete]
+        public void RemoveCollegeById(int id)
+        {
+            service.RemoveCollegeById(id);
+        }
+    }
+}
---
+++ Multilayer/webapi_projekt/Controllers/StudentController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Data.SqlClient;
+using Project.Service.Common;
+using Project.Service;
+using Project.Model;
+namespace Zadatak_web_api.Controllers
+{
+
+    public class StudentController : ApiController
+    {
+        public StudentController() { }
+        protected IStudentService service = new StudentService();
+
+        [HttpGet]
+        public List<Student> GetAllStudents()
+        {
+            return service.GetAllStudents();
+        }
+        [HttpGet]
+        public Student GetStudentById(int id)
+        {
+            return service.GetAStudent(id);
+        }
+
+        [HttpPost]
+        public void SaveNewValue([FromBody] Student student)
+        {
+            service.SaveNewValue(student);
+        }
+
+        [HttpPut]
+        public void UpdateStudentAddress(int id, string address)
+        {
+            service.UpdateStudentAddress(id, address);
+        }
+
+        [HttpDelete]
+        public void RemoveStudentById(int id)
+        {
+            service.RemoveStudentById(id);
+        }
+    }
+}
