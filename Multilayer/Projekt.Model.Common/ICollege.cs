5626c41ad09569a500d518e37a90b15d65080c13
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-27 14:55:59 +0100

    Add files via upload

---
+++ Multilayer/Projekt.Model.Common/ICollege.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.Common
+{
+    public interface ICollege
+    {
+         int Id { get; set; }
+         string Name { get; set; }
+         string Address { get; set; }
+    }
+}
