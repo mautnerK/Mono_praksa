f9e7c4c6a498fb39c04efdb7ae30e0e69fe790d9
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-27 15:14:27 +0100

    Add files via upload

---
+++ Multilayer/projekt.service/StudentService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Repository;
+using Project.Model;
+using Project.Repository.Common;
+using Project.Service.Common;
+
+namespace Project.Service
+{
+    public class StudentService : IStudentService
+    {
+        public StudentService() { }
+
+        protected IStudentRepository repository = new StudentRepository();
+        public List<Student> GetAllStudents() { 
+            return repository.GetAllStudents();
+        }
+
+
+
+        public Student GetAStudent(int id) {
+            return repository.GetAStudent(id);
+        }
+
+        public void SaveNewValue(Student student) {
+            repository.SaveNewValue(student);
+        }
+
+        public void UpdateStudentAddress(int id, string address) {
+            repository.UpdateStudentAddress(id, address);
+        }
+
+        public void RemoveStudentById(int id) { 
+            repository.RemoveStudentById(id);
+        }
+    }
+}
