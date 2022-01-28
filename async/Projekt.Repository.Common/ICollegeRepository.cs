9fe92a7569189969377f902891ec916c6d1f4973
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-01-28 14:50:16 +0100

    Add files via upload

---
+++ async/Projekt.Repository.Common/ICollegeRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Model;
+namespace Project.Repository.Common
+{
+    public interface ICollegeRepository
+    {
+        Task<List<College>> GetAllCollegesAsync();
+        Task<College> GetCollegeByIdAsync(int id);
+
+        Task SaveNewValueAsync(College college);
+
+        Task UpdateCollegeAddressAsync(int id, string address);
+
+        Task RemoveCollegeByIdAsync(int id);
+    }
+}
---
+++ async/Projekt.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,22 @@
+﻿using Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repository.Common
+{
+    public interface IStudentRepository
+    {
+       Task<List<Student>> GetAllStudentsAsync();
+
+       Task<Student> GetAStudentAsync(int id);
+
+       Task SaveNewValueAsync(Student student);
+
+       Task UpdateStudentAddressAsync(int id, string address);
+
+       Task RemoveStudentByIdAsync(int id);
+    }
+}
