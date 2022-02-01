27759a4cf0417e4724e8c7da0849a12861e38f14
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-01 14:51:13 +0100

    Add files via upload

---
+++ Sorting,Filtering,Paging/Projekt.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,32 @@
+﻿using Project.Common;
+using Projekt.Common;
+using Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repository.Common
+{
+    public interface IStudentRepository
+    {
+       Task<List<Student>> GetAllStudentsAsync();
+
+       Task<Student> GetAStudentAsync(int id);
+
+       Task SaveNewValueAsync(Student student);
+
+       Task UpdateStudentAddressAsync(int id, string address);
+
+       Task RemoveStudentByIdAsync(int id);
+
+        Task<List<Student>> GetSortedStudentsAsync(Sorting sort);
+
+        Task<List<Student>> GetFilteredStudentsAsync(Filtering filter);
+
+        Task<List<Student>> GetPagesOfStudentsAsync(Pager page);
+
+     
+    }
+}
