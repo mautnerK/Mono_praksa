0930823b66a610b2f58206cbde3f520042f6f7cb
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-01 14:53:02 +0100

    Add files via upload

---
+++ Sorting,Filtering,Paging/Projekt.Model.Common/IStudent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Model.Common
+{
+    public interface IStudent
+    {
+         int Id { get; set; }
+         string Name { get; set; }
+         string Surname { get; set; }
+         string Address { get; set; }
+
+      
+
+    }
+}
