06de3ccb2efb59a4bcdca564d095ca792ac98786
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-01 14:49:21 +0100

    Add files via upload

---
+++ Sorting,Filtering,Paging/Projekt.Sevice.Common/ICollegeService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Common;
+using Project.Model;
+using Projekt.Common;
+
+namespace Project.Service.Common
+{
+    public interface ICollegeService
+    {
+        Task<List<College>>GetAllCollegesAsync();
+        Task<College> GetCollegeByIdAsync(int id);
+
+        Task SaveNewValueAsync(College college);
+
+        Task UpdateCollegeAddressAsync(int id, string address);
+
+        Task RemoveCollegeByIdAsync(int id);
+        Task<List<College>> GetSortedCollegesAsync(Sorting sort);
+
+        Task<List<College>> GetFilteredCollegesAsync(Filtering filter);
+
+        Task<List<College>> GetPagesOfCollegesAsync(Pager page);
+
+    }
+}
---
+++ Sorting,Filtering,Paging/Projekt.Sevice.Common/IStudentService.cs
@@ -0,0 +1,32 @@
+﻿using Project.Common;
+using Project.Model;
+using Projekt.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Common
+{
+    public interface IStudentService
+    {
+        Task<List<Student>> GetAllStudentsAsync();
+
+        Task<Student> GetAStudentAsync(int id);
+
+        Task SaveNewValueAsync(Student student);
+
+        Task UpdateStudentAddressAsync(int id, string address);
+
+        Task RemoveStudentByIdAsync(int id);
+
+        Task<List<Student>> GetSortedStudentsAsync(Sorting sort);
+
+        Task<List<Student>> GetFilteredStudentsAsync(Filtering filter);
+
+        Task<List<Student>> GetPagesOfStudentsAsync(Pager page);
+
+        
+    }
+}
