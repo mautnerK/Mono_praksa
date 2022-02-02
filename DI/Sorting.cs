df2f076603816f7e5b93e7835d287faba7ee8778
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:16:07 +0100

    Add files via upload

---
+++ DI/Sorting.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Common
+{
+    public class Sorting
+    {
+        public string OrderBy { get; set; }
+
+        public string SortOrder { get; set; }
+       
+    }
+}
