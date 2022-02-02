f378eecc95b80d9b7ae66393e13d5c00e3d64734
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:09:25 +0100

    Add files via upload

---
+++ DI/Projekt.Repository.Common/ICollegeRepository.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Project.Common;
+using Project.Model;
+using Projekt.Common;
+
+namespace Project.Repository.Common
+{
+    public interface ICollegeRepository
+    {
+
+        Task<College> GetCollegeByIdAsync(int id);
+
+        Task SaveNewValueAsync(College college);
+
+        Task UpdateCollegeAddressAsync(int id, string address);
+
+        Task RemoveCollegeByIdAsync(int id);
+
+        Task<List<College>> GetAllCollegesAsync(Sorting sort, Pager page, Filtering filter); 
+     
+    }
+}
---
+++ DI/Projekt.Repository.Common/IStudentRepository.cs
@@ -0,0 +1,26 @@
+﻿using Project.Common;
+using Projekt.Common;
+using Project.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Repository.Common
+{
+    public interface IStudentRepository
+    {
+       Task<List<Student>> GetAllStudentsAsync(Sorting sort, Pager page, Filtering filter);
+        Task<List<Student>> GetAllStudentsAsync();
+
+       Task<Student> GetAStudentAsync(int id);
+
+       Task SaveNewValueAsync(Student student);
+
+       Task UpdateStudentAddressAsync(int id, string address);
+
+       Task RemoveStudentByIdAsync(int id);
+       
+    }
+}
