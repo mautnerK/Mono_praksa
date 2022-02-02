19879ce06587f03f0afc4ee0af64d4060a8456e8
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:12:48 +0100

    Add files via upload

---
+++ DI/Projekt.Model/ModelDiModule.cs
@@ -0,0 +1,22 @@
+﻿using Autofac;
+using Project.Model;
+using Project.Model.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Projekt.Model
+{
+    public class ModelDiModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<Student>().As<IStudent>().InstancePerRequest();
+            builder.RegisterType<College>().As<ICollege>().InstancePerRequest();
+
+
+        }
+    }
+}
