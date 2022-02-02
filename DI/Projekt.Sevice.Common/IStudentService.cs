b624b8e0672780224a3dc0e249f8343ff1478d18
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:07:04 +0100

    Add files via upload

---
+++ DI/Projekt.Sevice.Common/IStudentService.cs
@@ -0,0 +1,26 @@
+﻿using Project.Common;
+using Project.Model;
+using Projekt.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project.Service.Common
+{
+    public interface IStudentService
+    {
+        Task<List<Student>> GetAllStudentsAsync(Sorting sort, Pager page, Filtering filter);
+
+        Task<Student> GetAStudentAsync(int id);
+
+        Task SaveNewValueAsync(Student student);
+
+        Task UpdateStudentAddressAsync(int id, string address);
+
+        Task RemoveStudentByIdAsync(int id);
+
+     
+    }
+}
