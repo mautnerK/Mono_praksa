2a2a0d92de448d8c3b657879503fa84aa55e20b5
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:06:19 +0100

    Add files via upload

---
+++ DI/App_Start/AutofacWebapiConfig.cs
@@ -0,0 +1,49 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System.Reflection;
+using System.Web.Http;
+using Projekt.Model;
+using projekt.service;
+using projekt.repository;
+
+namespace AutoFacWithWebAPI.App_Start
+{
+    public class AutofacWebapiConfig
+    {
+
+        public static IContainer Container;
+
+        public static void Initialize(HttpConfiguration config)
+        {
+            Initialize(config, RegisterServices(new ContainerBuilder())); 
+        }
+
+
+        public static void Initialize(HttpConfiguration config, IContainer container)
+        {
+            config.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+
+        
+
+
+        private static IContainer RegisterServices(ContainerBuilder builder)
+        {
+            //Register your Web API controllers.  
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+
+           
+
+            builder.RegisterModule(new ServiceDIModule());
+            builder.RegisterModule(new RepositoryDIModule());
+
+
+            //Set the dependency resolver to be Autofac.  
+            Container = builder.Build();
+
+            return Container;
+        }
+
+    }
+}
---
+++ DI/App_Start/Bootstrapper.cs
@@ -0,0 +1,18 @@
+﻿using AutoFacWithWebAPI.App_Start;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace web_api_projekt.App_Start
+{
+    public class Bootstrapper
+    {
+        public static void Run()
+        {
+            //Configure AutoFac  
+            AutofacWebapiConfig.Initialize(GlobalConfiguration.Configuration);
+        }
+    }
+}
