f73f23cfa5f7ff9927914001e900e9b3de4eff04
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:10:19 +0100

    Add files via upload

---
+++ DI/RepositoryDIModule.cs
@@ -0,0 +1,20 @@
+﻿using Autofac;
+using Project.Repository;
+using Project.Repository.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace projekt.repository
+{
+    public class RepositoryDIModule : Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<StudentRepository>().As<IStudentRepository>().InstancePerRequest();
+            builder.RegisterType<CollegeRepository>().As<ICollegeRepository>().InstancePerRequest();
+        }
+    }
+}
