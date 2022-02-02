c7848fe79c46e2758f2177bf606332bb79071502
mautnerK <97747509+mautnerK@users.noreply.github.com>
2022-02-02 16:16:53 +0100

    Add files via upload

---
+++ DI/Projekt.Common/Pager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PagedList;
+namespace Projekt.Common
+{
+    public class Pager
+    {   
+        public int ItemsPerPage { get; set; }
+
+        public int PageNumber { get; set; }
+
+    }
+}
